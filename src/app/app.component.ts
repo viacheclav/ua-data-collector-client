aa37642c5128cc7fdbab5623147c0297c8f555ee
slavik <wayonmail@gmail.com>
2017-06-18 01:33:21 +0200

    Add possibility to show/hide part of data on the page

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -10,7 +10,17 @@ import {CurrencyRateGeneral} from "./currency-rate-general";
 })
 export class AppComponent implements OnInit {
   ngOnInit(): void {
-    this.getData('kyiv');
+    let cityFromStorage = localStorage.getItem('city');
+    let showCurrencyStorage = localStorage.getItem('showCurrency');
+    let showWeather1Storage = localStorage.getItem('showWeather1');
+    let showWeather2Storage = localStorage.getItem('showWeather2');
+
+    this.showCurrency = showCurrencyStorage ? showCurrencyStorage === "true" : true;
+    this.showWeather1 = showWeather1Storage ? showWeather1Storage === "true" : true;
+    this.showWeather2 = showWeather2Storage ? showWeather2Storage === "true" : true;
+
+
+    this.getData(cityFromStorage ? cityFromStorage : 'kyiv');
   }
 
   weatherDataDefault: any;
@@ -20,6 +30,9 @@ export class AppComponent implements OnInit {
   currencyRateDataGeneral: CurrencyRateGeneral[];
   currencyRateDataGeneralAll: CurrencyRateGeneral[];
   city: string;
+  showCurrency: boolean = true;
+  showWeather1: boolean = true;
+  showWeather2: boolean = true;
 
   constructor(private heroService: HeroService) {
   }
@@ -29,11 +42,20 @@ export class AppComponent implements OnInit {
       return;
     }
     this.city = city;
-    this.heroService.getWeatherDefault(city, 'dvfhgk').then(data => this.showWeatherDefault(data));
-    this.heroService.getWeatherDefault(city, 'khggty').then(data => this.weatherDataDefault2 = data);
-    this.heroService.getCurrencyRate().then(data => this.showCurrencyRate(data));
-    let currency: string = city === 'krakow' ? 'PLN' :'UAH';
-    this.heroService.getCurrencyRateGeneral(currency).then(data => this.showCurrencyRateUAH(data))
+    localStorage.setItem('city', this.city);
+    if (this.showCurrency) {
+      let currency: string = this.city === 'krakow' ? 'PLN' :'UAH';
+      if (currency === 'UAH'){
+        this.heroService.getCurrencyRate().then(data => this.showCurrencyRate(data));
+      }
+      this.heroService.getCurrencyRateGeneral(currency).then(data => this.showCurrencyRateUAH(data));
+    }
+    if (this.showWeather1) {
+      this.heroService.getWeatherDefault(this.city, 'dvfhgk').then(data => this.weatherDataDefault = data);
+    }
+    if (this.showWeather2){
+      this.heroService.getWeatherDefault(this.city, 'khggty').then(data => this.weatherDataDefault2 = data);
+    }
   }
 
   showCurrencyRateUAH(data: any): void{
@@ -72,7 +94,33 @@ export class AppComponent implements OnInit {
     });
   }
 
-  showWeatherDefault(data: any): void {
-    this.weatherDataDefault = data;
+  showOrHideCurrency(showCurrency: boolean){
+    this.showCurrency = showCurrency;
+    localStorage.setItem('showCurrency', ""+showCurrency);
+    if (showCurrency){
+      let currency: string = this.city === 'krakow' ? 'PLN' :'UAH';
+      if (currency === 'UAH'){
+        this.heroService.getCurrencyRate().then(data => this.showCurrencyRate(data));
+      }
+      this.heroService.getCurrencyRateGeneral(currency).then(data => this.showCurrencyRateUAH(data));
+    }
   }
+
+
+  showOrHideWeather1(showWeather1: boolean){
+    this.showWeather1 = showWeather1;
+    localStorage.setItem('showWeather1', ""+showWeather1);
+    if (showWeather1){
+      this.heroService.getWeatherDefault(this.city, 'dvfhgk').then(data => this.weatherDataDefault = data);
+    }
+  }
+
+  showOrHideWeather2(showWeather2: boolean){
+    this.showWeather2 = showWeather2;
+    localStorage.setItem('showWeather2', ""+showWeather2);
+    if (showWeather2){
+      this.heroService.getWeatherDefault(this.city, 'khggty').then(data => this.weatherDataDefault2 = data);
+    }
+  }
+
 }
